fc63145bd55f935b781c1ec95f5b2c58bc9a8841
Stephan <ssomers42@gmail.com>
2023-05-30 20:33:27 -0700

    added logic to map positioning

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -7,14 +7,17 @@ export const Map = ({ ipAddress }) => {
   const [lat, setLat] = useState(32.7679);
   const [long, setLong] = useState(-117.1235);
 
-  console.log(ipAddress);
-  console.log(lat, long);
-
+  //Updates MapContainer when ipAddress is updated
   const FlyMapOnUpdate = ({ center, zoom }) => {
     const map = useMap();
 
     useEffect(() => {
+      //set flag to prevent race conditions
+      let ignore = false;
+
+      //get location
       const getLocation = async () => {
+        console.log('location run');
         const apiKey = 'at_FcUoYu5h9NCfBwMyuelMKPInX8nwo';
         const geoAPI = `https:geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`;
 
@@ -22,13 +25,20 @@ export const Map = ({ ipAddress }) => {
         const locationData = await locationResponse.json();
 
         //Have to set state inside of getLocation since it is wrapped in a useEffect
-        setLat(locationData.location.lat);
-        setLong(locationData.location.lng);
+        if (!ignore) {
+          setLat(locationData.location.lat);
+          setLong(locationData.location.lng);
 
-        //MapContainer props are immutable. Use useMap hook to flyTo when the ipAddress changes
-        map.flyTo(center, zoom);
+          //MapContainer props are immutable. Use useMap hook to flyTo when the ipAddress changes
+          map.flyTo(center, zoom);
+        }
       };
-      getLocation();
+      ipAddress && getLocation();
+
+      //reset variable
+      return () => {
+        ignore = true;
+      };
     }, [ipAddress]);
   };
 
@@ -38,7 +48,7 @@ export const Map = ({ ipAddress }) => {
         center={[lat, long]}
         zoom={15}
         scrollWheelZoom={false}
-        style={{ height: '80rvh' }}>
+        style={{ height: '80vh' }}>
         <TileLayer
           attribution={
             '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
