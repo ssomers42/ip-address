3af5f082d264a051581cf0a81eb79fe71a74055c
Stephan <ssomers42@gmail.com>
2023-06-02 16:45:30 -0700

    API key env var

--- src/helpers/getIpData.jsx
+++ src/helpers/getIpData.jsx
@@ -1,7 +1,7 @@
 //takes in an ipAddress and fetches the geo API to return an ipData object
 export const getIpData = async (ipAddress) => {
   console.log('getting Data');
-  const apiKey = 'at_TAgc8qfjNVNSO8AMMtu2QIWkp516F';
+  const apiKey = process.env.API_KEY;
   const geoAPI = ipAddress
     ? `https:geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`
     : `https:geo.ipify.org/api/v2/country,city?apiKey=${apiKey}`;
