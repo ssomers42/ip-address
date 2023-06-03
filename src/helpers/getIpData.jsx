43ba5d9e2f5ce9d29f0511f24f520df552e13b79
Stephan <ssomers42@gmail.com>
2023-06-02 17:03:47 -0700

    API comments

--- src/helpers/getIpData.jsx
+++ src/helpers/getIpData.jsx
@@ -1,6 +1,8 @@
 //takes in an ipAddress and fetches the geo API to return an ipData object
 export const getIpData = async (ipAddress) => {
   console.log('getting Data');
+  //Netlify requires VITE_ prefix on API key
+  //Vite requires import.meta.env instead of process.env
   const apiKey = import.meta.env.VITE_API_KEY;
   const geoAPI = ipAddress
     ? `https:geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`
