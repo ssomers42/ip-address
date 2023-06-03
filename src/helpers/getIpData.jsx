dbf995d285405876ed378813a3b2000de1d555c4
Stephan <ssomers42@gmail.com>
2023-06-02 18:15:40 -0700

    testing again

--- src/helpers/getIpData.jsx
+++ src/helpers/getIpData.jsx
@@ -3,9 +3,9 @@ export const getIpData = async (ipAddress) => {
   console.log('getting Data');
   //Netlify requires VITE_ prefix on API key
   //Vite requires import.meta.env instead of process.env
-  console.log(geoAPI);
   const apiKey = import.meta.env.VITE_API_KEY;
   const geoAPI = `https:geo.ipify.org/api/v2/country,city?apiKey=${apiKey}&ipAddress=${ipAddress}`;
+  console.log(geoAPI);
 
   const ipInfoResponse = await fetch(geoAPI).catch((err) => console.log(err));
   const ipData = await ipInfoResponse.json();
